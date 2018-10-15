36c2045b9ea2ab0688e27a183815035bbdc1fbab
Julian <juli_fula@hotmail.com>
2018-10-15 15:11:34 -0300

    Fix ej1

--- Ej1/Ex1.pm
+++ Ej1/Ex1.pm
@@ -4,24 +4,28 @@
 
 use strict;
 use warnings;
+use Getopt::Long;
 use Bio::Seq;
 use Bio::SeqIO;
 use Bio::PrimarySeqI;
 
-my $file_name = shift || 'sequence.gb';     # Obtiene el nombre del archivo por línea de comandos, o usa 'sequence.gb' por default. 
+my $file_name = 'sequence.gb';     # Nombre del archivo default = 'sequence.gb'
+
+GetOptions ('f=s' => \$file_name);      # Si se ingresa otro nombre de archivo por linea de comandos. (Ej: --f nombre_de_archivo)
+
 my $input_file = Bio::SeqIO->new(-file => $file_name,   # Abre el archivo GenBank
                                  -format => 'genbank'
                                 );
+$file_name = substr $file_name, 0, -3; 
 my $output_file = Bio::SeqIO->new( -file => ">$file_name.fas",  # Genera el archivo Fasta de resultado
-                                   -format => 'fasta',
+                                  -format => 'fasta',
                                  );
 
 while (my $seq = $input_file->next_seq) {   # Lee cada secuencia del archivo Input
     if(!$seq->validate_seq($seq->seq)) {    # Valida la secuencia
-        print "SECUENCIA INVALIDA";
-        die;
+        die "************ ERROR: SECUENCIA INVALIDA";
     }
     my $traduccion = $seq->translate(-orf => 1, complete => 1, -start => 'atg', -throw => 1); # Traduce a amino ácidos
     $output_file->write_seq($traduccion);   # Escribe la traducción en el archivo
-    print $trad->seq, "\n";     # Imprime por pantalla cada traducción
+    print "> Secuencia: ", "\n", $traduccion->seq, "\n";     # Imprime por pantalla cada traducción
 }
