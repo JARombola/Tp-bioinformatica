a9c6a5fb024b23bc41f7cc9c094a511d20478b60
Julian <juli_fula@hotmail.com>
2018-10-15 20:21:13 -0300

    Ej1: Archivo con 6 marcos de lectura.

--- Ej1/Ex1.pm
+++ Ej1/Ex1.pm
@@ -18,14 +18,22 @@ my $input_file = Bio::SeqIO->new(-file => $file_name,   # Abre el archivo GenBan
                                 );
 $file_name = substr $file_name, 0, -3; 
 my $output_file = Bio::SeqIO->new( -file => ">$file_name.fas",  # Genera el archivo Fasta de resultado
-                                  -format => 'fasta',
-                                 );
+                                   -format => 'fasta',
+                                  );
 
 while (my $seq = $input_file->next_seq) {   # Lee cada secuencia del archivo Input
     if(!$seq->validate_seq($seq->seq)) {    # Valida la secuencia
         die "************ ERROR: SECUENCIA INVALIDA";
     }
-    my $traduccion = $seq->translate(-orf => 1, complete => 1, -start => 'atg', -throw => 1); # Traduce a amino ácidos
+    my $traduccion = $seq->translate(-orf => 1, -complete => 1, -start => 'atg', -throw => 1); # Traduce a amino ácidos
     $output_file->write_seq($traduccion);   # Escribe la traducción en el archivo
     print "> Secuencia: ", "\n", $traduccion->seq, "\n";     # Imprime por pantalla cada traducción
-}
+
+
+    # ARCHIVO CON LOS 6 MARCOS DE LECTURA
+    my @prots = Bio::SeqUtils->translate_6frames($seq);
+    my $marcos = Bio::SeqIO->new( -file => ">marcos.fas",
+                                   -format => 'fasta',
+                                  );
+    $marcos->write_seq(@prots);
+ }
